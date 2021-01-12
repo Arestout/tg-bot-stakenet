51d9dbbf1d1dfdcc4eff9d84931a26de25afa0a0
Anton Kablukov <antoha7@gmail.com>
2021-01-12 09:16:06 +0100

    add fetch to keepAlive for keeping heroku running

--- src/utils/keepAlive.js
+++ src/utils/keepAlive.js
@@ -8,6 +8,7 @@ const pingRequest = {
 
 function keepAlive(webSocket) {
   const timeout = 50000;
+  fetch('https://arcane-fjord-86920.herokuapp.com/');
   webSocket.send(JSON.stringify(pingRequest));
   timerId = setTimeout(keepAlive.bind(this, webSocket), timeout);
 }
