44fc4a8f2277e6ebea3e8b64ccd57105f11b7766
Anton Kablukov <antoha7@gmail.com>
2021-01-13 21:04:21 +0100

    remove unnecessary code in socket connect

--- src/lib/sockets/connect.js
+++ src/lib/sockets/connect.js
@@ -10,8 +10,6 @@ const { keepAlive, cancelKeepAlive } = require('../../utils/keepAlive');
 const telegram = new Telegram(TELEGRAM_TOKEN);
 
 const chatId = CHAT_ID;
-// const chatId = process.env.CHAT_TEST_ID;
-const chatId = process.env.CHAT_ID;
 
 const connectToSocket = (exchange, address) => {
   const webSocket = new WebSocket(address);
