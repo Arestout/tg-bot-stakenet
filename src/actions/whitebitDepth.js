db3371d6559cf65e25c489b5ea25f47e87c6ac48
Anton Kablukov <antoha7@gmail.com>
2021-01-18 11:33:56 +0100

    refactor market depth action with isFetching flag and loading message

--- src/actions/whitebitDepth.js
+++ src/actions/whitebitDepth.js
@@ -3,14 +3,26 @@ const convertToImage = require('../lib/htmlToImage');
 
 const whitebitUsdtDepth = (bot) => {
   bot.action('whitebit-usdt', async (ctx) => {
+    isFetching = false;
+
+    if (isFetching) {
+      return;
+    }
+
     try {
+      const { message_id } = await ctx.reply('Loading market depth...');
+      isFetching = true;
       const response = await getWhitebitMarketDepth();
       const image = await convertToImage(response);
+
+      ctx.deleteMessage(message_id);
 
       return ctx.replyWithPhoto({ source: image });
     } catch (error) {
       console.log(error.message);
       ctx.reply('Something went wrong, please try again');
+    } finally {
+      isFetching = false;
     }
   });
 };
